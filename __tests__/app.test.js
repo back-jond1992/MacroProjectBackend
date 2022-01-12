a22b831265bee26b7aa7a6aefb1027109a90b09d
back-jond1992 <b.jond7@outlook.com>
2022-01-12 15:29:57 +0000

    sets up test env with mocha/chai

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,2 +1,20 @@
-const expect = require("chai").expect;
+const chai = require("chai");
+const expect = chai.expect;
 const request = require("supertest");
+
+const testFuncs = require("../testFuncs");
+
+helloWorld = testFuncs.helloWorld();
+
+console.log(process.env.NODE_ENV);
+console.log(helloWorld);
+
+describe("test", function () {
+  describe("helloWorld()", function () {
+    it("should return hello world", function () {
+      console.log(process.env.NODE_ENV);
+      console.log(helloWorld);
+      expect(helloWorld).to.equal("hello world");
+    });
+  });
+});
